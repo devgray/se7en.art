40aeabf22e1f41d211dec52745b47a7b656d8908
Lolivy Palmera <itsmekhulet@gmail.com>
2023-07-29 22:17:51 +0800

    initial commit

---
+++ scrollup.js
@@ -0,0 +1,19 @@
+// Get the button
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("myBtn").style.display = "block";
+    window.alert(mybutton);
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
